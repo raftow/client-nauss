e62bcf130d47b13451032f15e295de66dffa51fc
rafiq boubaker <rafik.towati@gmail.com>
2026-01-01 17:13:47 +0300

    work on nauss project

--- extra/hierarchy_level-nauss.php
+++ extra/hierarchy_level-nauss.php
@@ -1,13 +1,11 @@
 <?php
 $hierarchy_level = [];
 
-if(((!$current_domain) or ($current_domain == 25)) // admission
-   and
-   ((!$current_module) or ($current_module == 1282)))
-{
-    $hierarchy_level[100]=['ar'=> 'مدير قبول', 'en'=> 'Admission director'];
-    $hierarchy_level[400]=['ar'=> 'مشرف قبول', 'en'=> 'Admission supervisor'];
-    $hierarchy_level[800]=['ar'=> 'موظف قبول', 'en'=> 'Admission employee'];
-    $hierarchy_level[999]=['ar'=> 'موظف مراجعة وثائق', 'en'=> 'Document review employee'];
+if (((!$current_domain) or ($current_domain == 25)) and  // admission
+        ((!$current_module) or ($current_module == 1282))) {
+    $hierarchy_level[50] = ['ar' => 'مدير التطبيق', 'en' => 'Application manager'];
+    $hierarchy_level[100] = ['ar' => 'مدير القبول', 'en' => 'Admission manager'];
+    // $hierarchy_level[400] = ['ar' => 'مشرف قبول', 'en' => 'Admission supervisor'];
+    $hierarchy_level[999] = ['ar' => 'موظف قبول', 'en' => 'Admission employee'];
+    // $hierarchy_level[999] = ['ar' => 'موظف مراجعة وثائق', 'en' => 'Document review employee'];
 }
-
