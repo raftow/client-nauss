2c2279485c23523c57ce8d7c50c395c6df41b023
rafiq boubaker <rafik.towati@gmail.com>
2026-01-05 10:11:19 +0300

    work on nauss project

---
+++ extra/application_category-nauss.php
@@ -0,0 +1,10 @@
+<?php
+$lookup = [];
+$lookup[1] = [
+    'ar' => 'الحاصلين على تقدير جيد جدا',
+    'en' => 'Graduates with Very Good grade',
+];
+$lookup[2] = [
+    'ar' => 'الحاصلين على تقدير أقل من جيد جدا',
+    'en' => 'Graduates less than Very Good grade',
+];
