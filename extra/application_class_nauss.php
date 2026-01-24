c3c725fc9cb45ad34bf5e8440c18f8cd61b16bea
rafiq boubaker <rafik.towati@gmail.com>
2026-01-24 16:33:33 +0300

    work on nauss project

--- extra/application_class_nauss.php
+++ extra/application_class_nauss.php
@@ -33,6 +33,22 @@ class ApplicationClassNauss
         return $lookup;
     }
 
+
+    public static function getWorkflowCategoryLookup()
+    {
+        $lookup = [];
+        $lookup[1] = [
+            'ar' => 'الحاصلين على تقدير جيد جدا فأعلى',
+            'en' => 'Graduates with Very Good grade or higher',
+        ];
+        $lookup[2] = [
+            'ar' => 'الحاصلين على تقدير أقل من جيد جدا',
+            'en' => 'Graduates less than Very Good grade',
+        ];
+
+        return $lookup;
+    }
+
     /**
      * @param Application $applicationObj
      * @param ApplicationDesire $desireObj
@@ -47,5 +63,23 @@ class ApplicationClassNauss
         }
         // @todo : case 2 not implemented for the moment waiting Amjad give explanation 
         return $ncObj->getVal("study_funding_status_id") + 2;
+    }
+
+
+    /**
+     * @param Application $applicationObj
+     * @param ApplicationDesire $desireObj
+     */
+
+    public static function calcWorkflowCategoryOf($applicationObj, $desireObj)
+    {
+        if (!$applicationObj) return 2;
+        if (!$desireObj) return 2;
+        $aqObj = $desireObj->het("applicant_qualification_id");
+        if (!$aqObj) return 2;
+        $gsObj = $aqObj->het("grading_scale_id");
+        if (!$gsObj) return 2;
+
+        return ($gsObj->getVal("level") >= 7) ? 1 : 2;
     }
 }
